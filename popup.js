b6d454e0c0b7dc3c2953784a789cc782664226a3
Thomas Mosmant <thomas@luko.eu>
2020-11-22 11:09:11 +0100

    No need for jquery

--- popup.js
+++ popup.js
@@ -2,12 +2,6 @@ let fetchButton = document.getElementById("fetch");
 
 fetchButton.onclick = function (element) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(
-      null,
-      { file: "jquery-3.5.1.min.js" },
-      function () {
-        chrome.tabs.executeScript(null, { file: "fetch-script.js" });
-      }
-    );
+    chrome.tabs.executeScript(null, { file: "fetch-script.js" });
   });
 };
