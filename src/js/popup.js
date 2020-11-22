e0ef22a1753ef583ab9ee2443757b3c6095b57ee
Thomas Mosmant <thomas@luko.eu>
2020-11-22 12:20:50 +0100

    Structure

--- src/js/popup.js
+++ src/js/popup.js
@@ -2,6 +2,6 @@ let exportButton = document.getElementById("export");
 
 exportButton.onclick = function (element) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(null, { file: "export-script.js" });
+    chrome.tabs.executeScript(null, { file: "./src/js/export-script.js" });
   });
 };
